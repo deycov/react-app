5b107a820e6122aff0f75ba59eab429040a75ba5
Adrian Restrepo <adriale0902@gmail.com>
2023-07-12 15:12:17 -0400

    Agregado de custom hook de localstorage

--- src/App.js
+++ src/App.js
@@ -6,23 +6,12 @@ import { List } from './components/list';
 import { Item } from './components/item';
 import { Search } from './components/search';
 import { Button } from './components/button';
+import { useLocalStorage } from './components/useLocalStorage';
 
 function App() {
-  // Local storage: proceso de almacenamiento local en un navegador simula ser un backend justamente para almacenar datos
-  const dataLocalStorage = localStorage.getItem('TODOS_V1');
-  let infoLocal;
-
-  if(!dataLocalStorage){
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    infoLocal = [];
-  }else{
-    infoLocal = JSON.parse(dataLocalStorage);
-  }
   
-  // Variables de estado
-  const [todos, setTodos] = React.useState(infoLocal); 
-  // Manipulacion de informacion con estados de react
-  // Seria como guardar la información del backend para renderizarla en el front 
+  const [todos, addNewTodos] = useLocalStorage('TODOS_V1',[]);
+
   const [searchValue, setSearchValues] = React.useState(''); // useState hace referencia a la informacion inicial que obtendrá react en sus estados
   // Variables de estado *Búsqueda* lo que ingresan
 
@@ -32,8 +21,6 @@ function App() {
   if(totalTodos === 0){
     greatings = "Felicidades por terminar las tareas";
   }
-
-   
 
   const searched = todos.filter((todo)=>{
     const todoText = todo.text.toLowerCase();
@@ -45,14 +32,14 @@ function App() {
     const newData = [...todos];
     const index = newData.findIndex((todo) => todo.text == data);
     newData[index].complete = true;
-    setTodos(newData);
+    addNewTodos(newData);
   }
 
   const erase = (data) => {
     const newData = [...todos];
     const index = newData.findIndex((todo) => todo.text == data);
     newData.splice(index,1);
-    setTodos(newData);
+    addNewTodos(newData);
   } 
   
   return (
